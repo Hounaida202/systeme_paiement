cc63390ca2b520c475cc25745d2f8e6495698ee4
Hounaida202 <etrichih@gmail.com>
2025-09-22 12:08:33 +0100

    l ajoit des getters et setters

--- untitled/src/Entities/Abonnement.java
+++ untitled/src/Entities/Abonnement.java
@@ -1,29 +1,79 @@
 package Entities;
 
 public abstract class Abonnement {
-  private String idAbonnement;
-  private String nomService;
-  private double montantMensuel;
-  private String dateDebut;
-  private String dateFin;
-  private String statut;
+    private String idAbonnement;
+    private String nomService;
+    private double montantMensuel;
+    private String dateDebut;
+    private String dateFin;
+    private Statut statut; // utilisation de l'enum comme type
 
-  public enum statut {active,suspendu,resilie};
+    public enum Statut {
+        ACTIVE, SUSPENDU, RESILIE
+    }
 
-  public Abonnement(String idAbonnement,
-                    String nomService,
-                    double montantMensuel,
-                    String dateDebut,
-                    String dateFin,
-                    String statut) {
+    public Abonnement(String idAbonnement,
+                      String nomService,
+                      double montantMensuel,
+                      String dateDebut,
+                      String dateFin,
+                      Statut statut) {
 
-      this.idAbonnement = idAbonnement;
-      this.nomService = nomService;
-      this.montantMensuel = montantMensuel;
-      this.dateDebut = dateDebut;
-      this.dateFin = dateFin;
-      this.statut=statut;
+        this.idAbonnement = idAbonnement;
+        this.nomService = nomService;
+        this.montantMensuel = montantMensuel;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.statut = statut;
+    }
 
-  }
+    // ---------- Getters et Setters ----------
 
+    public String getIdAbonnement() {
+        return idAbonnement;
+    }
+
+    public void setIdAbonnement(String idAbonnement) {
+        this.idAbonnement = idAbonnement;
+    }
+
+    public String getNomService() {
+        return nomService;
+    }
+
+    public void setNomService(String nomService) {
+        this.nomService = nomService;
+    }
+
+    public double getMontantMensuel() {
+        return montantMensuel;
+    }
+
+    public void setMontantMensuel(double montantMensuel) {
+        this.montantMensuel = montantMensuel;
+    }
+
+    public String getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(String dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public String getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(String dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public Statut getStatut() {
+        return statut;
+    }
+
+    public void setStatut(Statut statut) {
+        this.statut = statut;
+    }
 }
--- untitled/src/Entities/AbonnementAvecEngagement.java
+++ untitled/src/Entities/AbonnementAvecEngagement.java
@@ -1,17 +1,27 @@
 package Entities;
 
-public class AbonnementAvecEngagement extends Abonnement{
- private String dureeEngagementMois;
+public class AbonnementAvecEngagement extends Abonnement {
+    private String dureeEngagementMois;
 
     public AbonnementAvecEngagement(String idAbonnement,
                                     String nomService,
                                     double montantMensuel,
                                     String dateDebut,
                                     String dateFin,
-                                    String statut,
+                                    Statut statut,
                                     String dureeEngagementMois) {
 
         super(idAbonnement, nomService, montantMensuel, dateDebut, dateFin, statut);
+        this.dureeEngagementMois = dureeEngagementMois;
+    }
+
+    // ---------- Getters et Setters ----------
+
+    public String getDureeEngagementMois() {
+        return dureeEngagementMois;
+    }
+
+    public void setDureeEngagementMois(String dureeEngagementMois) {
         this.dureeEngagementMois = dureeEngagementMois;
     }
 }
