7e4cdda19a49953d3dbdfe357713a0b46e92eddc
Hounaida202 <etrichih@gmail.com>
2025-09-22 11:49:47 +0100

    la creation des attributs de la class paiement

--- untitled/src/Entities/Paiement.java
+++ untitled/src/Entities/Paiement.java
@@ -1,5 +1,11 @@
 package Entities;
 
 public class Paiement {
+   private String idPaiement;
+   private String idAbonnement;
+   private String dateEcheance;
+   private String datePaiement;
+   private String typePaiement;
+   private enum statut {paye,nonpaye,retard};
 
 }
