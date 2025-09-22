15120a7fa532dd67146cc3514cbbe071fcf3c3f8
Hounaida202 <etrichih@gmail.com>
2025-09-22 12:11:44 +0100

    la creation des getters setters et constructeur de la classe paiement

--- untitled/src/Entities/Paiement.java
+++ untitled/src/Entities/Paiement.java
@@ -1,11 +1,78 @@
 package Entities;
 
 public class Paiement {
-   private String idPaiement;
-   private String idAbonnement;
-   private String dateEcheance;
-   private String datePaiement;
-   private String typePaiement;
-   private enum statut {paye,nonpaye,retard};
+    private String idPaiement;
+    private String idAbonnement;
+    private String dateEcheance;
+    private String datePaiement;
+    private String typePaiement;
+    private Statut statut; // utilisation de l'enum
 
+    public enum Statut {
+        PAYE, NONPAYE, RETARD
+    }
+
+    // ---------- Constructeur ----------
+    public Paiement(String idPaiement,
+                    String idAbonnement,
+                    String dateEcheance,
+                    String datePaiement,
+                    String typePaiement,
+                    Statut statut) {
+        this.idPaiement = idPaiement;
+        this.idAbonnement = idAbonnement;
+        this.dateEcheance = dateEcheance;
+        this.datePaiement = datePaiement;
+        this.typePaiement = typePaiement;
+        this.statut = statut;
+    }
+
+    // ---------- Getters et Setters ----------
+    public String getIdPaiement() {
+        return idPaiement;
+    }
+
+    public void setIdPaiement(String idPaiement) {
+        this.idPaiement = idPaiement;
+    }
+
+    public String getIdAbonnement() {
+        return idAbonnement;
+    }
+
+    public void setIdAbonnement(String idAbonnement) {
+        this.idAbonnement = idAbonnement;
+    }
+
+    public String getDateEcheance() {
+        return dateEcheance;
+    }
+
+    public void setDateEcheance(String dateEcheance) {
+        this.dateEcheance = dateEcheance;
+    }
+
+    public String getDatePaiement() {
+        return datePaiement;
+    }
+
+    public void setDatePaiement(String datePaiement) {
+        this.datePaiement = datePaiement;
+    }
+
+    public String getTypePaiement() {
+        return typePaiement;
+    }
+
+    public void setTypePaiement(String typePaiement) {
+        this.typePaiement = typePaiement;
+    }
+
+    public Statut getStatut() {
+        return statut;
+    }
+
+    public void setStatut(Statut statut) {
+        this.statut = statut;
+    }
 }
