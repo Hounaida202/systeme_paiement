3e56d4bb64ba1c17b2a143792f2f941093cd9eb6
Hounaida202 <etrichih@gmail.com>
2025-09-22 12:06:31 +0100

    la creation des constructeurs des classes abonnement et abonnement avec engagement

--- untitled/src/Entities/Abonnement.java
+++ untitled/src/Entities/Abonnement.java
@@ -6,6 +6,24 @@ public abstract class Abonnement {
   private double montantMensuel;
   private String dateDebut;
   private String dateFin;
-  private enum statut {active,suspendu,resilie};
+  private String statut;
+
+  public enum statut {active,suspendu,resilie};
+
+  public Abonnement(String idAbonnement,
+                    String nomService,
+                    double montantMensuel,
+                    String dateDebut,
+                    String dateFin,
+                    String statut) {
+
+      this.idAbonnement = idAbonnement;
+      this.nomService = nomService;
+      this.montantMensuel = montantMensuel;
+      this.dateDebut = dateDebut;
+      this.dateFin = dateFin;
+      this.statut=statut;
+
+  }
 
 }
--- untitled/src/Entities/AbonnementAvecEngagement.java
+++ untitled/src/Entities/AbonnementAvecEngagement.java
@@ -2,4 +2,16 @@ package Entities;
 
 public class AbonnementAvecEngagement extends Abonnement{
  private String dureeEngagementMois;
+
+    public AbonnementAvecEngagement(String idAbonnement,
+                                    String nomService,
+                                    double montantMensuel,
+                                    String dateDebut,
+                                    String dateFin,
+                                    String statut,
+                                    String dureeEngagementMois) {
+
+        super(idAbonnement, nomService, montantMensuel, dateDebut, dateFin, statut);
+        this.dureeEngagementMois = dureeEngagementMois;
+    }
 }
